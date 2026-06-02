707d3138f0f239e224b71ae54fc40994cda235db
opallrock6-crypto <opallrock6@gmail.com>
2026-06-02 14:44:16 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ TOKEN = os.getenv("TOKEN")
 # ID TELEGRAM ADMIN
 # DAPAT DARI @userinfobot
 # =========================================
-ADMIN_ID = 8496137364
+ADMIN_ID = 1713040623
 
 # =========================================
 # LINK PRODUK
